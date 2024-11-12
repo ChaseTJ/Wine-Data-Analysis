42fd495108ba77d4f3e68c3e83b9be8f72f2caa9
ChaseTJ <chasej@utexas.edu>
2024-11-12 17:12:49 -0600

    Adding some initial files

---
+++ Wine_Data_Analysis.py
@@ -0,0 +1,13 @@
+'''
+Wine Data Analysis
+
+Chase Johnson
+'''
+
+# %%
+import gspread as gc
+
+# %% Importing wine rating from google sheet
+
+wine_ratings = gc.open_by_key('1EZHsM7t7N9HXrkUtZHQWTOB4ktEv09WjW8R2wP5osAM')
+# %%
