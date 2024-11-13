af365b509ab8c3c3f51d1547de363e864d468bdd
ChaseTJ <chasej@utexas.edu>
2024-11-12 18:36:46 -0600

    Reading in from the google sheet

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Wine-Data-Analysis
 Analyzes personal wine ratings and generates wine recommendations through statistical analysis of the ratings and a dataset of wines.
+
+WineDataset.csv is from https://www.kaggle.com/datasets/elvinrustam/wine-dataset?resource=download
--- Wine_Data_Analysis.py
+++ Wine_Data_Analysis.py
@@ -5,9 +5,26 @@ Chase Johnson
 '''
 
 # %%
-import gspread as gc
+import gspread
+import pandas as pd
+from google.oauth2.service_account import Credentials
 
 # %% Importing wine rating from google sheet
 
-wine_ratings = gc.open_by_key('1EZHsM7t7N9HXrkUtZHQWTOB4ktEv09WjW8R2wP5osAM')
+scopes = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
+
+creds = Credentials.from_service_account_file("wine-analysis-441522-ec24032b1416.json", scopes=scopes)
+
+gc = gspread.authorize(creds)
+
+spreadsheet = gc.open("Wine Master Sheet")
+
+worksheet = spreadsheet.worksheet("Wine Stats")
+
+data = worksheet.get_all_records(head=2)
+
+df = pd.DataFrame(data)
 # %%
